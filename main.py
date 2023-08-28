03a5284283c7811e7a4764e01dbf5b4f316f973b
Lovilek <wingeddemon2274@gmail.com>
2023-08-28 16:47:27 +0500

    .

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from twilio.rest import Client
+
+end_point = "https://api.openweathermap.org/data/2.8/onecall"
+api_key = "5b520806aa9bf507a36923f28952e16a"
+account_sid = 'AC4c067a311cb216e274a864024eab570e'
+auth_token = '1ee3076cf97c142875a4b5f9b9c5c50b'
+
+params = {
+    "lat": 43.669039,
+    "lon": 51.180117,
+    "exclude": "daily,current,minutely,alerts",
+    "appid": api_key
+}
+response = requests.get(url=end_point, params=params)
+response.raise_for_status()
+list_weather = [item for item in response.json()["hourly"] if response.json()["hourly"].index(item) < 12]
+rain = False
+for item in list_weather:
+    if item["weather"][0]["id"] < 700:
+        rain = True
+
+if rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="It's going to rain today. Take an umbrella☂",
+        from_='+18642522521',
+        to='+77787329544'
+    )
+    print(message.status)
+
